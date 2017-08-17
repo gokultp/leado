ea2af39cbaf76a5a3ecdb881dd3206e086b1f9f4
Gokul T P <tp.gokul@gmail.com>
2017-08-17 21:32:14 +0530

    testing basic setup

--- server/test/filters.test.js
+++ server/test/filters.test.js
@@ -1,3 +1,10 @@
 var chai = require('chai');
 var expect = require('chai').expect;
 
+describe('Testing filter conditions', function(){
+    it('Test', function (done) {
+        expect(1).to.equal(1)
+        done()
+    })
+})
+    
