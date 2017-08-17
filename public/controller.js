fb760ad51dc25c2baae3fcca9c29f87e2ea2387f
Gokul T P <tp.gokul@gmail.com>
2017-08-18 02:59:01 +0530

    showing existing hooks

--- public/controller.js
+++ public/controller.js
@@ -1,7 +1,13 @@
+angular
+.module('leado', [])
+
 angular.module('leado')
-    .controller('mainController',['$scope', 'mainFactory',,  function ($scope, mainFactory) {
-        mainFactory.getHooks(1, 10).then(function (data) {
-            console.log(data);
+    .controller('mainController',['$scope', 'mainFactory',  function ($scope, mainFactory) {
+        $scope.hooks    = [];
+        mainFactory.getHooks(1, 10).then(function (temp) {
+            if(temp.data.status){
+                $scope.hooks    = temp.data.data;
+            }
         })
     }])
 
--- server/apis/hooks/controller.js
+++ server/apis/hooks/controller.js
@@ -3,8 +3,9 @@ const DEFAULT_PAGE      = 1;
 
 var HooksLib        = require('../../library/hooks.server.library');
 exports.getAllHooks =   function (req, res) {
-    var limit   = req.query.limit || DEFAULT_LIMIT;
-    var page    = req.query.page || DEFAULT_PAGE;
+    console.log(req.query)
+    var limit   = Number(req.query.limit || DEFAULT_LIMIT);
+    var page    = Number(req.query.page || DEFAULT_PAGE);
 
     return HooksLib.getAllWebhooks(page, limit).then(function (data) {
         res.json({status: true, data: data})
