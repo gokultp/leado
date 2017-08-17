feb94d5a56149ee224f62cb1942c89e7c5e4c4ea
Gokul T P <tp.gokul@gmail.com>
2017-08-17 21:42:39 +0530

    test cases for filter

--- server/test/filters.test.js
+++ server/test/filters.test.js
@@ -1,10 +1,35 @@
-var chai = require('chai');
-var expect = require('chai').expect;
+var chai      = require('chai');
+var expect    = require('chai').expect;
+var Filter    = require('../helpers/filters.server.helper');
+
+var TestData    = [
+    {
+        case : 'Testing eq operator',
+        filter: [[{dataField: 'price', condition: 'eq', referenceVal: 10}]],
+        data : {price: 10},
+        result : true
+    },
+    {
+        case : 'Testing eq operator negetive',
+        filter: [[{dataField: 'price', condition: 'eq', referenceVal: 10}]],
+        data : {price: 9},
+        result : false
+    }
+
+]
+
+
 
 describe('Testing filter conditions', function(){
-    it('Test', function (done) {
-        expect(1).to.equal(1)
-        done()
+
+    TestData.forEach(function (test) {
+        it(test.case, function (done) {
+            var filter      = new Filter(test.filter);
+            result          = filter.apply(test.data);
+            expect(result).to.equal(test.result)
+            done()
+        })
     })
+    
 })
     
